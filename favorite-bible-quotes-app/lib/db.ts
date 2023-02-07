c8dfca40043b4c4189e142577254f7336ba4ae83
Quaid Rodgers <business.quaidrodgers@gmail.com>
2023-02-07 13:08:09 -0600

    added interface for type-safe celebrity querying, added build/ to gitignore

--- favorite-bible-quotes-app/lib/db.ts
+++ favorite-bible-quotes-app/lib/db.ts
@@ -2,6 +2,12 @@ import postgres from 'postgres'
 import dotenv from 'dotenv'
 dotenv.config()
 
+export interface Celebrities {
+    id: number,
+    full_name: string,
+    image_url: string
+
+}
 
 export function getPgSQLClient() { 
     return process.env.COCKROACH_DB_CONNECTION_STRING ? postgres(process.env.COCKROACH_DB_CONNECTION_STRING) : undefined 
