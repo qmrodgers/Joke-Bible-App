d48fdb6ee043fb3d16fc46d0e930527d86313226
Quaid Rodgers <business.quaidrodgers@gmail.com>
2023-03-21 13:19:40 -0500

    set to revoke image object urls on completion

--- favorite-bible-quotes-app/src/components/Celeb.tsx
+++ favorite-bible-quotes-app/src/components/Celeb.tsx
@@ -77,6 +77,7 @@ export function Celeb() {
       console.log('running next celeb data')
           if (!celebSet.current) { console.log('no celeb set'); return false;}
       console.log(`setting celebrity ${celebSet.current[0].celebrity.full_name}`)
+      if (currentCeleb?.celebrity.image_url) URL.revokeObjectURL(currentCeleb?.celebrity.image_url);
       setCurrentCeleb(celebSet.current[0]);
       iterateCelebritySet();
       return true;
